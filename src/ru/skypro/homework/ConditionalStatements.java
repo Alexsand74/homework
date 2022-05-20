9ca2bacb32de16d15ec25e6497c92f7c8db0ee9f
User <1974alexsand@gmail.com>
2022-05-20 13:59:57 +0400

    Домашнее задание 1.3 Условные операторы

--- src/ru/skypro/homework/ConditionalStatements.java
+++ src/ru/skypro/homework/ConditionalStatements.java
@@ -6,8 +6,9 @@ public class ConditionalStatements {
 
     public static void main(String[] args) {
        System.out.println();
-       WebsiteBank();// задание 1
+       WebsiteBank();          //Задание 1
        YearOptsionnayaSystem();//Задание 2
+       LeapYear();             //Задание 3
     }
     public static void WebsiteBank (){
         /*
@@ -25,6 +26,7 @@ public class ConditionalStatements {
         А для Android так:
         «Установите версию приложения для Android по ссылке».
          */
+        System.out.println(" Задание 1");
         int clientOS = 0;
         if ( clientOS == 0 ){
             System.out.println("Установите версию приложения для iOS по ссылке");
@@ -51,6 +53,7 @@ public class ConditionalStatements {
         Для обозначения года создания используйте переменную clientDeviceYear, где необходимо указать любой желаемый год.
         **Важно**: вложенность больше двух этажей не допускается (условный оператор внутри условного оператора).
          */
+        System.out.println(" Задание 2");
         int clientOS = 0;
         int clientDeviceYear = 2012;
         if ( clientOS == 0 ){
@@ -65,6 +68,32 @@ public class ConditionalStatements {
             } else {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
+        }
+        System.out.println();
+    }
+    public static void LeapYear (){
+        /*
+        Задание 3
+        Напишите программу, которая определяет, является ли год високосным или нет.
+        Переменную года назовите year, в которую можно подставить значение интересующего нас года.
+        Например, 2021.
+        Программа должна определять, високосный год или нет, и выводить соответствующее сообщение:
+        « …. год является високосным».
+        или
+        «... год не является високосным».
+        Небольшая справка: високосным является каждый четвертый год, но не является каждый сотый.
+         Также високосным является каждый четырехсотый год.
+         */
+        System.out.println(" Задание 3");
+        int year = 2021;
+        System.out.println( year % 100 + " деление на 100");
+        System.out.println( year % 400 + " деление на 400");
+        System.out.println( year % 4 + " деление на 4");
+
+        if ((year % 4  == 0) && (year % 100 != 0) || (year % 400  == 0)){
+            System.out.println( year + " год является високосным");
+        } else {
+            System.out.println( year + " год не является високосным");
         }
         System.out.println();
     }
